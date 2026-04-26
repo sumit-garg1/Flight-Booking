6e83b5520d360553fefdbcc7b6ba801f540b91d7
sumit-garg1 <sumitgarg2fbd@gmail.com>
2026-04-26 12:52:21 +0530

    updated files

--- airline_reservation/settings.py
+++ airline_reservation/settings.py
@@ -35,6 +35,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
